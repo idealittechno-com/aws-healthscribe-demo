816d6732175fce84302b4df1cb14718a87b3b4c4
idealittechno <84897887+idealittechno-com@users.noreply.github.com>
2024-11-11 15:05:37 +0530

    Update TopNav.tsx

--- src/components/TopNav/TopNav.tsx
+++ src/components/TopNav/TopNav.tsx
@@ -143,7 +143,7 @@ export default function TopNav() {
             <TopNavigation
                 identity={{
                     href: '/',
-                    title: 'Amazon Web Services',
+                    title: 'Active Rockland Transcription Services',
                 }}
                 utilities={navUtils}
             />
